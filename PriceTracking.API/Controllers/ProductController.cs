db220e4e75a75ef7beb531e4ff051f92a1e49838
Fatih Afşin <50847253+fafsin495@users.noreply.github.com>
2023-08-29 22:54:04 +0300

    added product&custom base controller

---
+++ PriceTracking.API/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PriceTracking.Core.DTOs;
+using PriceTracking.Core.DTOs.ResponseDtos;
+using PriceTracking.Core.Models;
+using PriceTracking.Core.Services;
+
+namespace PriceTracking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _productService;
+
+        public ProductController(IMapper mapper, IService<Product> productService)
+        {
+            _mapper = mapper;
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _productService.GetAll();
+            var productDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Succes(200,productDtos));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Succes(200, productDto));
+        }
+
+
+    }
+}
--- PriceTracking.Core/Models/ProductFeature.cs
+++ PriceTracking.Core/Models/ProductFeature.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PriceTracking.Core.Models
 {
-    public class ProductFeature
+    public class ProductFeature:BaseEntity
     {
+        //[Key]
         public string ProductId { get; set; }
         public string ProductTitle { get; set; }
         public string ProductUrl { get; set; }
