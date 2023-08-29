13ae123ed1d165f4db45fe266a3e919e754032a6
Fatih Afşin <50847253+fafsin495@users.noreply.github.com>
2023-08-29 15:06:35 +0300

    Added core repositories and services

---
+++ PriceTracking.Core/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceTracking.Core
+{
+    public class Product:BaseEntity
+    {
+        public string ProductTitle{ get; set; }
+        public string ProductCategory { get; set; }
+        public decimal ProductPrice { get; set; }
+        public DateTime ProductDate{ get; set; }
+        public string ProductId { get; set; }
+        public string ProductUrl { get; set; }
+
+    }
+}
---
+++ PriceTracking.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace PriceTracking.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+    }
+}
